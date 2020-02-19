70272c2cc6a4c0176756d1c2e8149d6d2dfbe365
DEJ <eteilj@gmail.com>
2020-02-19 14:07:17 +0100

    Added Taco, Design. Modify DesignTacoController for handling form submission

---
+++ src/main/java/com/dej/tacoscloud/models/Design.java
@@ -0,0 +1,12 @@
+package com.dej.tacoscloud.models;
+
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Design {
+    private String name;
+    private List<Ingredient> ingredientList;
+}
